86c06e9aa5d3bb6cf122786861acb6ec607db38d
luizantonioo <125517343+luizantonioo@users.noreply.github.com>
2023-10-15 15:02:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+
+}
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+}
+function draw(){
+    image(video, 0, 0, 640, 480)
+    tint(tintcolor);
+    circle(10,10);
+    circle(10,480);
+    circle(480,480);
+    circle(10,480);
+}
+function tirarFoto(){
+
+}
+
+
+
+
+
+
+
+
+
+
