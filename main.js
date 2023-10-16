e0622a399388bbf7d979e6b7e6268012ca9a2233
luizantonioo <125517343+luizantonioo@users.noreply.github.com>
2023-10-16 16:52:26 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,20 +4,28 @@ function preload(){
 function setup(){
 canvas = createCanvas(300,300);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("Modelo iniciado!");
 }
 function draw(){
-    image(video, 0, 0, 640, 480)
-    tint(tintcolor);
-    circle(10,10);
-    circle(10,480);
-    circle(480,480);
-    circle(10,480);
+    image(video, 0, 0, 300,300)
 }
-function tirarFoto(){
-
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+    console.log("Nariz x = "+results[0].pose.nose.x);
+    console.log("Nariz y = "+results[0].pose.nose.y);
 }
-
-
+}
+function filtro(){
+    tintColor = document.getElementById("cor").value;
+}
 
 
 
